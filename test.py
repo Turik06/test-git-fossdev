05fb174316a9d52336773af8215c1628829edd67
Turik <turalsuleymanov2006@gmail.com>
2026-02-10 14:44:33 +0800

    add errors 2

--- test.py
+++ test.py
@@ -1,24 +1,29 @@
-from script import sum ,devide
+from script import devide, sum
+
+
 def test_sum():
     a = 1
     b = 2
     result = 3
-    assert sum(a,b) == result
+    assert sum(a, b) == result
+
 
 def test_devide():
     a = 2
     b = 4
     result = 0.5
-    assert devide(a,b) == result
+    assert devide(a, b) == result
+
 
 def test_devide_zero():
     a = 2
     b = 0
     try:
-        devide(a,b)
+        sum(a, b)
         assert False
     except ValueError as e:
-        print("Good")
+        print("Test (zero-devision) passed")
+
 
 if __name__ == "__main__":
     test_sum()
