176427496548077c44d149f8c2476e7306f232ec
turik06 <turalsuleymanov2006@gmail.com>
2026-04-03 16:54:19 +0800

    Keep only hw_pypi and README.md

--- testing/frameworks/tests/tax/__test_income.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from tax.income import calculate_tax
-
-def test_income_tax():
-    assert calculate_tax(100)==15
-    print("Test tax passed")
-
-if __name__== "__main__":
-    test_income_tax()
